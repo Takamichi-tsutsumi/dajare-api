1c0157d2b79263c72ec656b61d06e89c38eceea8
Takamichi-tsutsumi <1220t.takamichi@gmail.com>
2016-02-14 01:19:18 +0900

    [Remove: lines] * Lifecycle methods

--- dev-webpack-server.js
+++ dev-webpack-server.js
@@ -10,7 +10,8 @@ new WebpackDevServer(webpack(config), {
     publicPath: config.output.publicPath,
     hot: true,
     inline: true,
-    historyApiFallback: true
+    historyApiFallback: true,
+    watch: true
 }).listen(3000, '0.0.0.0', function(err, result) {
     if (err) {
         console.error(err);
--- static/assets/js/components/app.js
+++ static/assets/js/components/app.js
@@ -4,33 +4,10 @@ import axios from 'axios';
 
 export default class App extends Component {
 
-    componentWillMount() {
-        axios.get('http://0.0.0.0:1337/api/users/').
-        then( (data) =>{
-                console.log(data);
-                this.setState({'users': data.data});
-            }
-        );
-    }
-
-    renderUsers() {
-        if(!this.state.users) {
-            return '';
-        }
-
-        return (
-            this.state.users.map((user) => {
-                return <li>{user.name}</li>;
-            })
-        )
-    }
-
     render() {
         return (
             <div>
                 React simple starter
-                <ul>
-                </ul>
             </div>
         );
     }
