69d22e41b3aad968703b907e50e3ad80f760ab91
Takamichi-tsutsumi <1220t.takamichi@gmail.com>
2016-02-13 16:33:19 +0900

    [ADD: feature] *API test

--- static/assets/js/components/app.js
+++ static/assets/js/components/app.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import { Component } from 'react';
+import axios from 'axios';
 
 export default class App extends Component {
-  render() {
-    return (
-      <div>React simple starter</div>
-    );
-  }
+
+    componentWillMount() {
+        axios.get('http://0.0.0.0:1337/api/users/').
+        then( (data) =>{
+                console.log(data);
+                this.setState({'users': data.data});
+            }
+        );
+    }
+
+    renderUsers() {
+        if(!this.state.users) {
+            return '';
+        }
+
+        return (
+            this.state.users.map((user) => {
+                return <li>{user.name}</li>;
+            })
+        )
+    }
+
+    render() {
+        return (
+            <div>
+                React simple starter
+                <ul>
+                </ul>
+            </div>
+        );
+    }
 }
